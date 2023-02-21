7852a7dba61f533c5de231f0220bd605f729b96b
Charles Bousseau <cbousseau@anaconda.com>
2023-02-21 17:24:43 -0500

    skip additional flaky test

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,11 @@
 sed -i '/udp_multicast_join/d' ./test/test-list.h
 sed -i '/udp_multicast_join6/d' ./test/test-list.h
 
+# This particular test intermittently fails on osx
+if [ "${target_platform}" == 'osx-64' ]; then
+   sed -i '/fs_event_error_reporting/d' ./test/test-list.h
+fi
+
 # LIBTOOLIZE setting is required to workaround missing glibtoolize on OS X:
 # https://github.com/joyent/libuv/issues/1200
 LIBTOOLIZE=libtoolize sh ./autogen.sh
