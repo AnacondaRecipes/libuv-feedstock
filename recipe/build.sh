750063864811c623c9788d964399957e65ce2208
Charles Bousseau <cbousseau@anaconda.com>
2023-02-22 09:41:47 -0500

    skip more tess on osx-64

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,9 @@ sed -i '/udp_multicast_join6/d' ./test/test-list.h
 if [ "${target_platform}" == 'osx-64' ]; then
    # see https://github.com/libuv/libuv/issues/3862
    sed -i '/fs_event_error_reporting/d' ./test/test-list.h
+   sed -i '/fs_event_watch_dir/d' ./test/test-list.h
    sed -i '/fs_event_watch_dir_recursive/d' ./test/test-list.h
+   sed -i '/fs_event_close_in_callback/d' ./test/test-list.h
 fi
 
 # LIBTOOLIZE setting is required to workaround missing glibtoolize on OS X:
