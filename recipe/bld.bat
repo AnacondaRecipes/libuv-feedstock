5aea650cf6920b42d4039fa7ad91943d59c09ced
Serhii Kupriienko <skupriienko@anaconda.com>
2024-07-05 11:30:07 +0300

    Remove test of a static library

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,17 +1,7 @@
+echo ON
+
 cmake -LAH -G Ninja -S . -B build ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
   -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
 cmake --build build --target install --parallel %CPU_COUNT%
-
-:: pushd build
-::  ctest -C Debug --output-on-failure
-:: popd
-
-pushd build
-  ::cmake --build . --config Release --target install
-  move %LIBRARY_PREFIX%\lib\Release\uv.lib %LIBRARY_PREFIX%\lib\uv.lib
-  move %LIBRARY_PREFIX%\lib\Release\uv_a.lib %LIBRARY_PREFIX%\lib\uv_a.lib
-  move %LIBRARY_PREFIX%\lib\Release\uv.dll %LIBRARY_BIN%\uv.dll
-  rmdir /s /q %LIBRARY_PREFIX%\lib\Release
-popd
