0482758d32eb2e4efead62969a204f321a65cff9
Serhii Kupriienko <skupriienko@anaconda.com>
2024-07-05 11:18:57 +0300

    Fix bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,15 @@
-mkdir -p build
-pushd build
-  cmake -LAH -G Ninja -S . -B build ^
-        -DCMAKE_BUILD_TYPE=Release ^
-        -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-        -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-        -DBUILD_TESTING=ON
-        ..
-  cmake --build . --target build
-popd
+cmake -LAH -G Ninja -S . -B build ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
+cmake --build build --target install --parallel %CPU_COUNT%
 
 :: pushd build
 ::  ctest -C Debug --output-on-failure
 :: popd
 
 pushd build
-  cmake --build . --config Release --target install
+  ::cmake --build . --config Release --target install
   move %LIBRARY_PREFIX%\lib\Release\uv.lib %LIBRARY_PREFIX%\lib\uv.lib
   move %LIBRARY_PREFIX%\lib\Release\uv_a.lib %LIBRARY_PREFIX%\lib\uv_a.lib
   move %LIBRARY_PREFIX%\lib\Release\uv.dll %LIBRARY_BIN%\uv.dll
